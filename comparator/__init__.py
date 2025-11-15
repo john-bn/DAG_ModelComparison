e58cb11464d9cc00d3e74958dce1d2e86ae25445
John Needham <john.b.needham@gmail.com>
2025-11-15 01:44:22 -0600

    yup

--- comparator/__init__.py
+++ comparator/__init__.py
@@ -1,3 +1,3 @@
-from .comparator_tempdiff import compute_tempdiff_f
+from .tempdiff import compute_tempdiff_f
 from .plotting import plot_tempdiff_map, plot_airports
 from .util import major_airports_df
