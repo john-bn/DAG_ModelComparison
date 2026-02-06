875a5784878cf687708a891e200919a672596762
John Needham <john.b.needham@gmail.com>
2026-02-06 02:16:44 -0600

    yerp

--- comparator/__init__.py
+++ comparator/__init__.py
@@ -1,4 +1,4 @@
-from .tempdiff import compute_fielddiff
+from .fielddiff import compute_fielddiff
 from .plotting import plot_tempdiff_map_with_table, plot_airports
 from .util import major_airports_df
 from .normalize import normalize_model_key, herbie_kwargs_for, normalize_var_key, pick_data_varname_from_ds
